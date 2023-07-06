fc64429080df7b0fbf58ee45b6e3585f8f9ae7ad
“Jayant git config --global user.email “jayantsingh331@gmail.com <jayantsingh331@gmail.com>
2023-07-06 14:18:56 +0530

    add-tilt-on-image-projects

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,15 +2,26 @@ import React from 'react'
 import Profile from '../../assets/home.jpg'
 import optimize from '../../assets/home-optimize.jpg'
 import optimized from '../../assets/home-optimize-min.jpg'
-import optimized_min from '../../assets/home-low.jpg'
 import {Link} from 'react-router-dom'
 import { FaArrowRight } from 'react-icons/fa'
+import { Tilt } from 'react-tilt'
 import './Home.css'
 function Home() {
+  const defaultOptions = {
+    reverse:        true,  // reverse the tilt direction
+    max:            25,     // max tilt rotation (degrees)
+    perspective:    1000,   // Transform perspective, the lower the more extreme the tilt gets.
+    scale:          1,    // 2 = 200%, 1.5 = 150%, etc..
+    speed:          100,   // Speed of the enter/exit transition
+    transition:     true,   // Set a transition on enter/exit.
+    axis:           null,   // What axis should be disabled. Can be X or Y.
+    reset:          true,    // If the tilt effect has to be reset on exit.
+    easing:         "cubic-bezier(.03,.98,.52,.99)",    // Easing on enter/exit.
+  }
   return (
     <section className="home section grid">
-     <picture> 
-      <img loading='lazy' src={Profile} srcSet={`${optimized} ${200}w, ${optimize} ${500}w,` }  alt="Profile Picture" className="home_img" /> </picture>
+     <Tilt options={defaultOptions}> 
+      <img loading='lazy' src={Profile} srcSet={`${optimized} ${200}w, ${optimize} ${500}w,` }  alt="" className="home_img" /> </Tilt>
     <div className="home_content">
       <div className="home_data">
         <h1 className="home_title">
--- src/Pages/Portfolio/Portfolio.jsx
+++ src/Pages/Portfolio/Portfolio.jsx
@@ -2,14 +2,27 @@ import React from 'react'
 import { portfolio } from '../../data'
 import PortfolioItem from '../../Components/PortfolioItem'
 import './Portfolio.css'
+import { Tilt } from 'react-tilt'
 function Portfolio() {
+  const defaultOptions = {
+    reverse:        true,  // reverse the tilt direction
+    max:            30,     // max tilt rotation (degrees)
+    perspective:    1000,   // Transform perspective, the lower the more extreme the tilt gets.
+    scale:          1.1,    // 2 = 200%, 1.5 = 150%, etc..
+    speed:          600,   // Speed of the enter/exit transition
+    transition:     true,   // Set a transition on enter/exit.
+    axis:           null,   // What axis should be disabled. Can be X or Y.
+    reset:          true,    // If the tilt effect has to be reset on exit.
+    easing:         "cubic-bezier(.03,.98,.52,.99)",    // Easing on enter/exit.
+  }
   return (
     <section className="portfolio section">
       <h2 className="section_title portfolio-title">My <span>Portfolio</span></h2>
       <div className="portfolio_container container grid">
         {portfolio.map((item)=>{
-          return <PortfolioItem key={item.id}{...item}/>
-        })}
+          return <Tilt options={defaultOptions}><PortfolioItem key={item.id}{...item}/></Tilt>
+      
+})}
       </div>
     </section>
   )
